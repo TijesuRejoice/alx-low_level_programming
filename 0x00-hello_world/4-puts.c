7fcaf5302af67b04f5ef9ec8c7aa29057ab7be11
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-08 18:31:19 +0000

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-Serves as the entry point for the program
+ * Return: By default returns 0 if no errors otherwise returnd non-0 value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
