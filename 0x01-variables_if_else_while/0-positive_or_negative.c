f7bddd0d6a0894a1802af3e0c210dd543d59961a
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-09 16:19:19 +0000

    Execute

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%D is positive\n", n);
+		printf("d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%D is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
