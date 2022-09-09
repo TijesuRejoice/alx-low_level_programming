e0a7c4efd7485d9f007b580aa26b512e950e27f9
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-09 13:51:45 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**main- entry point of code, all codes are here
+ * using putchar only twice
+ * return successful
+ */
+int main()
+{
+	int ch;
+	for (ch='a'; ch<='z'; ch++)
+		putchar(ch);
+		putchar("\n");
+	return (1);
+}
+
