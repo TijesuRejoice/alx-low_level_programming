1684e67bb2772e3806515a0c4853debbca4eb321
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-09 18:48:56 +0000

    Alphabetss

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lower, upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+	putchar('\n');
+	return (0);
+}
