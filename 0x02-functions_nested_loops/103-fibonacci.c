e934f821506669512f8a8c2367850c77b9f57bae
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-13 20:26:13 +0000

    Fifteen

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j, k, l, m;
+
+	j = 1;
+	k = 2;
+	m = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			m = m _ j;
+		}
+		l = j + k;
+		j = k;
+		k = 1;
+	}
+	printf("%lu\n", m);
+	return (0);
+}
