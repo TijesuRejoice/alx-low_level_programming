ec3fd2158bb064e0de1ed36c1cbe0ee0a944bfe8
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-13 17:07:23 +0000

    Task ten

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints add two numbers
+ * @i: Print int i
+ * @k: Print int k
+ * Return: Always 0 (Success)
+ */
+int add(int i, int k)
+{
+	return (i + k);
+}
