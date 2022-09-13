1ab89d8c3c0f138f45245081b01ae71613213441
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-13 19:03:27 +0000

    Tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
+ * print_alphabet_x10 - prints a - z ten times
+ * 
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 	int i;
-	int j;
+	char j;
 
 	for (i = 0; i < 10; i++)
 	{
