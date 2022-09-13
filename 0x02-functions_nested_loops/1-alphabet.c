01639f058bc321a15aa4e529e83ed2c99bd6a5b8
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-13 14:38:23 +0000

    Task onccer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,21 +3,17 @@
 /**
  * main - writes c to stdout
  *
- *
- * Description - prints the alphabet in lowercase, followed by a new line
+ * Description - prints the alphabet in lowecase in a new line
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-	void print_alphabet(void);
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		char i = 'a';
-		+
-		while (i <= 'z')
-			_putchar('i');
-		_putchar('\n');
-		i++;
+		_putchar(i);
 	}
-	return (0);
+	_putchar('\n')
 }
