e1db822dc093e836cf5a3a6711319c4426819984
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-13 15:36:21 +0000

    Task three

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
