8a6451e0d751c81986145ee018a6cf85d7a18972
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-13 16:27:58 +0000

    Helpful file

---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,19 @@
+nclude <limits.h>
+#include "main.h"
+
+/**
+ *  * main - check the code for Holberton School students.
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+		int r;
+
+			r = print_sign(98);
+				if (r < 0)
+							r *= -1;
+					_putchar(r + '0');
+						_putchar('\n');
+							return (0);
+}
