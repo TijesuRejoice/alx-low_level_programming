50530596ff847bd46eb78745c65990a4e7167772
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-13 12:32:21 +0000

    Task one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+		_putchar('\n');
+	}
+	return (0);
+}
