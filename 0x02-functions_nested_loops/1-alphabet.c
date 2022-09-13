47b9022c7e21d7beb31a602d9d5c778227eb059f
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-13 12:38:42 +0000

    Task onee

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
-#include <alphabet.h>
+#include "alphabet.h"
 
 /**
- * main - entry point
+ * print_alphabet - entry point
  *
  * Description - prints the alphabet in lowercase, followed by a new line
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	print_alphabet();
 	{
