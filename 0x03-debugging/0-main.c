764a4e252d6c2c54f4f4e7d136f1c33628e4c648
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-14 13:51:50 +0000

    Task zero: debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - testing a function that prints P or N-vty of a integer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
