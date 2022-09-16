d0a6b3370935c7131727629761a333f9d2de2189
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-16 15:18:30 +0000

    Task seven

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal lines
+ * @n: number of times the character \ should be printed
+ */
+void print_diagonal(int n)
+{
+	int x, i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i < x; i++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
