deb94b5bf2cab1276e80df12c90d5eb91101decd
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-16 17:45:13 +0000

    Correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * print_triangle - prints a triangle, using the character #
+ * @size: The size of the triangle
  */
-
-void print_remaining_days(int month, int day, int year)
+void print_triangle(int size)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-	{
-		if (month > 2 && day >= 60)
-		{
-			day++;
-		}
+	int hash, index;
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
+	if (size > 0)
 	{
-		if (month == 2 && day == 60)
+		for (hash = 1; hash <= size; hash++)
 		{
-			printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			for (index = size - hash; index > 0; index--)
+				_putchar(' ');
+
+			for (index = 0; index < hash; index++)
+				_putchar('#');
+
+			if (hash == size)
+				continue;
+
+			_putchar('\n');
 		}
 	}
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
+ 
+int _purchar(char c);
 
 void positive_or_negative(int i);
 
