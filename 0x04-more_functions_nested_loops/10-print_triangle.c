c3d0610bbf9cb51c2cc7bfeb71274127baa44e08
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-16 15:45:39 +0000

    Task tenner

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,28 +2,28 @@
 
 /**
  * print_triangle create traingle
- * @size: params
+ * @size: size of triangle
  * Return: 0
  */
 void print_triangle(int size)
 {
-	int i = 0, j, n = size - 1;
+	int i, j;
 
-	if (size > 0)
+	for (i = 0; i < size; i++)
 	{
-		for (; i < size; i++)
+		for (j = 1; j < (size - i); j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				if (j < n)
-					_putchar(' ');
-				else
-					_putchar('#');
-			}
-			n--;
+			_putchar(' ');
+		}
+
+		for (j--; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i < (size - 1))
+		{
 			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n');
+	_putchar('\n');
 }
