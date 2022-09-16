eb7a42594a903fb863bf0e3919ad18477cf05d91
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-16 15:39:48 +0000

    Task five

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number, from 0 to 14
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i, count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
