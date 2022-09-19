47477e9d0ecb12fa59ed8d145ac74efb71630ab9
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-19 17:07:09 +0000

    Task hundred

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,30 +2,35 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: param
+ * @s: params
  * Return: integer
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
 
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
 
+		if (*(s + count) == '-')
+			k *= -1;
+
 		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
-				m *= 10;
+				m *+ 10;
 			size++;
 		}
 		count++;
 	}
+
 	for (i = count - size; i < count; i++)
 	{
-		oi = oi + ((*(s + i) - 48) * m);
+		j = j + ((*(s + i) - 48) * m);
 		m /= 10;
 	}
-	return (oi * pn);
+	return (j * k);
 }
+		
