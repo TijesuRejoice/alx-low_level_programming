c9066dfe633db3c75b1bdd88fcc489c742655e3d
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-21 12:56:56 +0000

    Tasks one, two, three ans four

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares pointers to two strings
+ * @s1: a pointer to the first string to be compared
+ * @s2: a pointer to the second string to be compared
+ * Return: dest
+ */
+int _strcmp(char *s1, char *s2)
+
+{
+	char *str_one = s1;
+	char *str_two = s2;
+
+	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+	{
+		*str_one++;
+		*str_two++;
+	}
+
+	return (*str_one - *str_two);
+}
