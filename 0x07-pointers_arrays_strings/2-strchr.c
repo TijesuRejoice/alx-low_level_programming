00bad4e447ae6f88de4a2fd1841c419d1f56d7dd
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-28 14:48:15 +0000

    Tasks one, two, three

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to search
+ * Return: pointer to first occurence of c/
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: initial segment
+ * @accept: accepted bytes
+ * Return: the number of accepted  bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		count = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				count = 0;
+				break;
+			}
+		}
+		if (count == 1)
+			break;
+	}
+	return (i);
+}
