729cb69815e5e659a53b0d4e35b87aa4abdcdadb
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-09-30 14:10:37 +0000

    Task one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the number of arguments passed into it
@@ -6,8 +7,10 @@
  * @argv: array that contains the program command line arguements
  * Return: 0
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	(void) argv;
+	printf("%i\n", argc - 1);
+
 	return (0);
 }
