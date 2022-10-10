a748fbca63c1e8c395761cf46ba995b964a212e0
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-10-10 15:42:33 +0000

    Done task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dog
+ * @d: struct dog
+ * Return: no return
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
