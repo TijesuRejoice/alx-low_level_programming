5ae1207b2b4d3cb59054dcdbfb56c977874c0593
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-10-25 10:44:08 +0000

    Task zero

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,16 +2,20 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: head of linked list
+ * @h: linked list of type listint_t to print
+ *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t size;
+	size_t num = 0;
 
-	for (size = 0; h != NULL; size++)
+	while (h)
 	{
 		printf("%d\n", h->n);
+		num++;
 		h = h->next;
 	}
+	return (num);
+}
 }
