f8bcabc111ae10e6575b8e002de7786f925854f4
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-11-01 09:56:36 +0000

    Task one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int o, w, len = 0;
 
-	if (fd == -1)
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(fd);
+	close(o);
 
 	return (1);
 }
