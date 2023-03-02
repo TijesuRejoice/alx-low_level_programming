b444ecf21ecc815163ada0fd1cab1b0659453d2f
“TijesuRejoice” <tijesuadeleke94gmail.com>
2023-03-02 11:05:38 +0000

    Task threee

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-	new = malloC(sizeof(hash_node_t));
+	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
 		free(value_copy);
